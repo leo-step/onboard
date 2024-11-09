8304fd7f7e281ad472bcf42dd0973ac34d76947e
sgarimel <61368121+sgarimel@users.noreply.github.com>
2024-11-09 14:00:26 -0500

    splitrepo w/ mongodb

--- splitrepo.py
+++ splitrepo.py
@@ -2,6 +2,13 @@ import requests
 import re
 import json
 from utils import system_prompt, user_prompt, openai_json_response
+from prompts import select_candidate_files,  give_files_list, create_problem_context, send_context
+from clients import db_client
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+load_dotenv()
+
 url = "https://uithub.com/TigerAppsOrg/PrincetonCourses?accept=text%2Fplain&maxTokens=10000000"
 
 response = requests.get(url)
@@ -18,100 +25,30 @@ def parse_files(file_data):
     return files_dict
 
 parsed_data = parse_files(response.text)
-# files = list(parse_files.keys())
+files = list(parsed_data.keys())
 
-# @system_prompt
-# def select_candidate_files():
-#     return '''You will be given a list of file names from a repository. Your overall goal
-#     is to create coding questions that cover core concepts from the repository. You need to
-#     choose the best files to base your questions on. Return a JSON '''
+response = openai_json_response([
+    select_candidate_files(),
+    give_files_list(files)
+])
 
-# @user_prompt
-# def give_files_list():
-#     pass
+for i in tqdm(range(len(response["files"]))):
+    file_name = response["files"][i]
+    file_content = parsed_data[file_name]
 
+    responseLLM = openai_json_response([
+        create_problem_context(), 
+        send_context(file_content)
+    ], model="gpt-4o")
 
-
-# exit()
-
-file_name = "/controllers/endpoints/instructor.js"
-file_content = parsed_data[file_name]
-
-@system_prompt
-def create_problem_context(): 
-    return '''We are creating an app to teach employees our codebase. 
-    We want to pick an easy line from a given file and produce a problem description for it that 
-    contains enough context (variable names, method calls from the line) and ask the user to only 
-    rewrite a small part of the line of code. We want employees to code this line of code exactly as it is as 
-    an answer to our question.
-
-    
-     We want you to create a problem description for an easy line from the given file for the employee to 
-     then rewrite - choose this easy line yourself, but make sure to include any relevant parameter values 
-     that the user needs to know from the line of code in the problem description. 
-    
-    For example, if you chose the line of code "res.set('Cache-Control', 'public, max-age=86400').json(instructor)",
-    then include that the maximum age is 86400 in the problem description and the parameter Cache-Control.
-
-    For the line that you pick, you ***CANNOT*** pick multiple lines, ***ONLY*** pick single lines. There should be 
-    ***NO*** new line characters in the line. 
-
-    For example, this line of code would NOT be a valid line of code to replace because there are multiple lines in 
-    the code: 
-
-    if ((typeof (req.params.id) === 'undefined') || isNaN(req.params.id)) {
-    res.sendStatus(400)
-    return
-    }
-
-    But, you could have just if ((typeof (req.params.id) === 'undefined') || isNaN(req.params.id)) { as the line of code
-    because this is only one line. 
-    
-    Try avoiding doing trivial things like initializing libraries or exporting too much (focus on logic and operations). Do
-    this for at least 3 different lines of code across the file (preferably not subsequeny lines).
-    Output the line of code and the problem description in the following format:
-    {
-        title: title of the file
-        description[]: array of strings with a description of each task
-        line[]: an array of strings with the line of code of each task
-    }
-
-    '''
-
-@user_prompt
-def send_context(context: str):
-    return context
-
-
-responseLLM = openai_json_response([
-    create_problem_context(), 
-    send_context(file_content)
-], model="gpt-4o")
-
-responseLLM["question"] = file_content
-filtered_data = list(filter(lambda x: x[1] in responseLLM["question"], zip(responseLLM["description"], responseLLM["line"])))
-
-# Unzip the filtered result back into description and lines
-filtered_description, filtered_lines = zip(*filtered_data) if filtered_data else ([], [])
-
-# Convert back to lists (since zip returns tuples)
-responseLLM["description"] = list(filtered_description)
-responseLLM["line"] = list(filtered_lines)
-
-print(responseLLM)
-# questionSplit = responseLLM["question"].split(responseLLM["line"])
-# lineLength = len(responseLLM["line"])
-
-# questionFITB = [
-#     questionSplit[0],
-#     "Input(" + str(lineLength) + ")",
-#     questionSplit[1]
-# ]
-
-# print(questionFITB)
-# print(responseLLM['description'])
-
-
+    responseLLM["question"] = file_content
+    filtered_data = list(filter(lambda x: x[1] in responseLLM["question"], zip(responseLLM["descriptions"], responseLLM["lines"])))
+    filtered_description, filtered_lines = zip(*filtered_data) if filtered_data else ([], [])
+    responseLLM["descriptions"] = list(filtered_description)
+    responseLLM["lines"] = list(filtered_lines)
+    responseLLM["question_number"] = i
+    responseLLM["url"] = url
+    db_client["questions"].insert_one(responseLLM)
 '''
 {
     "_id": 123,
