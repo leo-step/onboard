7db103cf63714624d2fe6632d253c0d4bc7312d6
sgarimel <61368121+sgarimel@users.noreply.github.com>
2024-11-09 20:03:42 -0500

    prompts updated with partials

--- prompts.py
+++ prompts.py
@@ -5,23 +5,23 @@ import random
 def create_problem_context(): 
     
     return '''We are creating an app to teach employees our codebase. 
-    We want to pick an easy line from a given file and produce a problem description for it that 
-    contains enough context (variable names, method calls from the line) and ask the user to only 
-    rewrite a small part of the line of code. We want employees to code this line of code exactly as it is as 
+    We want to pick an easy partial or complete line of code from a given file and produce a problem description for it that 
+    contains enough context (variable names, method calls in that method) and ask the user to only 
+    rewrite a small part of the selected partial or complete line of code. We want employees to code this line of code exactly as it is as 
     an answer to our question.
 
     
-     We want you to create a problem description for an easy line from the given file for the employee to 
-     then rewrite - choose this easy line yourself, but make sure to include any relevant parameter values 
-     that the user needs to know from the line of code in the problem description. 
+     We want you to create a problem description for an easy partial/complete line of code from the given file for the employee to 
+     then rewrite - choose this easy partial/complete line yourself, but make sure to include any relevant parameter values 
+     that the user needs to know from the partial/complete line of code in the problem description. 
     
-    For example, if you chose the line of code "res.set('Cache-Control', 'public, max-age=86400').json(instructor)",
+    For example, if you chose the partial/complete line of code  "res.set('Cache-Control', 'public, max-age=86400').json(instructor)",
     then include that the maximum age is 86400 in the problem description and the parameter Cache-Control.
 
     For the line that you pick, you ***CANNOT*** pick multiple lines, ***ONLY*** pick single lines. There should be 
     ***NO*** new line characters in the line. 
 
-    For example, this line of code would NOT be a valid line of code to replace because there are multiple lines in 
+    For example, this partial/complete line of code  would NOT be a valid partial/complete line of code  to replace because there are multiple lines in 
     the code: 
 
     if ((typeof (req.params.id) === 'undefined') || isNaN(req.params.id)) {
@@ -41,6 +41,11 @@ def create_problem_context():
         lines[]: an array of strings with the line of code of each task
     }
 
+    Other guidelines: 
+    - Don't ask users to write complex error messages or urls, or anything that they wouldn't know or that's subjective.
+      Focus on testing their logic skills. 
+    - focus on only picking partial lines of code, only some complete lines of code.
+    
     Make sure the problem descriptions are actually good enough to solve the problem. For example, if you select
     a line with a print statement, tell the user the exact string to print otherwise its not possible to reasonably
     fill in the blank.
